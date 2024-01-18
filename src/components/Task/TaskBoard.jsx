ea11c0d38e01e84596e3d2dafa9d3f04753b951b
programmerNizam <mdnizam12109@gmail.com>
2024-01-19 00:15:40 +0600

    Create task state

--- src/components/Task/TaskBoard.jsx
+++ src/components/Task/TaskBoard.jsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import SearchBox from "./SearchBox";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
 
 export default function TaskBoard() {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Learn React",
+    description:
+      "I want to learn React such than I can treat it like I can treat it like I can treat it like I can treat it like",
+    priority: "High",
+    isFavorite: true,
+  };
+  const [tasks, setTasks] = useState([defaultTask]);
+
   return (
     <section className="mb-20" id="tasks">
       <div className="container mx-auto">
         <SearchBox />
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskAction />
-          <TaskList />
+          <TaskList tasks={tasks} />
         </div>
       </div>
     </section>
