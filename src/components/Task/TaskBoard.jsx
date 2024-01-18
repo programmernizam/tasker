71d63a08b88c8df97a8f2ffe2dd4074c930d099b
programmerNizam <mdnizam12109@gmail.com>
2024-01-19 00:26:53 +0600

    Add react icons on project

--- src/components/Task/TaskBoard.jsx
+++ src/components/Task/TaskBoard.jsx
@@ -11,7 +11,7 @@ export default function TaskBoard() {
       "I want to learn React such than I can treat it like I can treat it like I can treat it like I can treat it like",
     tags: ["web", "react", "js"],
     priority: "High",
-    isFavorite: true,
+    isFavorite: false,
   };
   const [tasks, setTasks] = useState([defaultTask]);
 
