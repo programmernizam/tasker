1a91eebaef8bda9f1119743170c92bb90e40f1ce
programmerNizam <mdnizam12109@gmail.com>
2024-01-19 00:42:34 +0600

    Start Add Task Handler Function

--- src/components/Task/TaskBoard.jsx
+++ src/components/Task/TaskBoard.jsx
@@ -15,12 +15,17 @@ export default function TaskBoard() {
   };
   const [tasks, setTasks] = useState([defaultTask]);
 
+  // Add Task Handler
+  const handleAddTask = () => {
+    console.log("Add Task");
+  };
+
   return (
     <section className="mb-20" id="tasks">
       <div className="container mx-auto">
         <SearchBox />
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskAction />
+          <TaskAction handleAddTask={handleAddTask} />
           <TaskList tasks={tasks} />
         </div>
       </div>
