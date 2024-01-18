6f18e7b8249ccb21c621e247696fb88c23a510dd
programmerNizam <mdnizam12109@gmail.com>
2024-01-18 21:32:57 +0600

    Building Footer Completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,11 @@
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="py-6 md:py-8">
+      <div className="container mx-auto">
+        <p className="text-center text-xs text-[#6A6A6A] lg:text-sm">
+          Copyright ©2024 | All rights reserved by ProgrammerNizam
+        </p>
+      </div>
+    </footer>
+  );
 }
