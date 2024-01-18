6e0e0077058aa3b81d476937484ea912778a12d9
programmerNizam <mdnizam12109@gmail.com>
2024-01-19 00:06:40 +0600

    Create task component

--- src/components/Task/TaskBoard.jsx
+++ src/components/Task/TaskBoard.jsx
@@ -6,12 +6,9 @@ export default function TaskBoard() {
   return (
     <section className="mb-20" id="tasks">
       <div className="container mx-auto">
-        {/* Search Box */}
         <SearchBox />
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          {/* Task Action */}
           <TaskAction />
-          {/* Task List */}
           <TaskList />
         </div>
       </div>
