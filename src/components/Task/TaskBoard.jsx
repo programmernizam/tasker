0bdfd1d15a2e9b738ab791d5bf89fd380ed35933
programmerNizam <mdnizam12109@gmail.com>
2024-01-18 21:47:10 +0600

    search box and task action design added

--- src/components/Task/TaskBoard.jsx
+++ src/components/Task/TaskBoard.jsx
@@ -1,11 +1,13 @@
 import SearchBox from "./SearchBox";
+import TaskAction from "./TaskAction";
 
 export default function TaskBoard() {
   return (
     <section className="mb-20" id="tasks">
       <div className="container mx-auto">
         <SearchBox />
-        
+        <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+          <TaskAction />
           <div className="overflow-auto">
             <table className="table-fixed overflow-auto xl:w-full">
               <thead>
