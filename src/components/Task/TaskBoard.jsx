7a62ff476b1cd4a9d20b4546a81a3b4180d6d1af
programmerNizam <mdnizam12109@gmail.com>
2024-01-18 21:49:09 +0600

    Task List component created

--- src/components/Task/TaskBoard.jsx
+++ src/components/Task/TaskBoard.jsx
@@ -1,5 +1,6 @@
 import SearchBox from "./SearchBox";
 import TaskAction from "./TaskAction";
+import TaskList from "./TaskList";
 
 export default function TaskBoard() {
   return (
@@ -8,9 +9,10 @@ export default function TaskBoard() {
         {/* Search Box */}
         <SearchBox />
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-            {/* Task Action */}
+          {/* Task Action */}
           <TaskAction />
-          
+          {/* Task List */}
+          <TaskList />
         </div>
       </div>
     </section>
