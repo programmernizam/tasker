5f822cb1f079913b8f54fd4598418bc058357c67
programmerNizam <mdnizam12109@gmail.com>
2024-04-06 19:52:36 +0600

    [Added] Task Reducer

---
+++ src/reducers/TaskReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+
+}
+
+const TaskReducer = (state, action) => {
+    switch (action.type) {
+
+        default:
+            return state
+    }
+}
+
+export { TaskReducer, initialState }
